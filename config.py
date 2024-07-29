43f3c16d23754998cd8cc1fc888cfac54ffbbc53
sehzadi4455 <sehzadi2217037@gmail.com>
2024-07-29 20:17:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7355244469:AAGTeTvl6sqnmbQqKG__dyBlU5LC7r1Qzvw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Rajsaahu")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1354861792")
